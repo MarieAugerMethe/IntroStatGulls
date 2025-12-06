dc56adb3b2ba63ced18ca9b02921f35db2ad81e4
Marie Auger-Methe <marie.augermethe@gmail.com>
2025-12-06 10:50:37 -0800

    Ronin gull summer project

---
+++ R/gulls_data.R
@@ -0,0 +1,20 @@
+#' This data set contains information about gulls and Toxoplasmosis infections.
+#'
+#' @docType data
+#' @name gulls_data
+#' @usage data(gulls_data)
+#' @format A data frame with 29 rows and 10 columns:
+#' \describe{
+#'   \item{BirdID}{character combination associated with an individual bird}
+#'   \item{CWS_band}{number combination associated with an individual leg band}
+#'   \item{Year}{Year of capture}
+#'   \item{Month}{Month of capture}
+#'   \item{Day}{Day of capture}
+#'   \item{Toxoplasmosis}{Whether or not bird has the disease Toxoplasmosis. (1=Yes,0=No)}
+#'   \item{Bird_weight_kg}{Bird weight at time of capture in killograms}
+#'   \item{Head_length_mm}{Bird head length at time of capture in millimeters}
+#'   \item{Bill_depth_mm}{Bird bill depth at time of capture in millimeters}
+#'   \item{Bill_length_mm}{Bird bill length at time of capture in millimeters}
+#' }
+#' @keywords data sets
+"gulls_data"
